df43cb579c7d6d77aebd53be54ca81426d7f2dbd
Josh Blotter <joshblotter@gmail.com>
2024-02-29 09:07:40 -0700

    trying to rig up custom theme, making the css work slightly better than before.

--- Angular/src/app/calculator/calculator.component.ts
+++ Angular/src/app/calculator/calculator.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatRadioModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatRadioModule, MatButtonModule, MatIconModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
--- Angular/src/app/history/history.component.ts
+++ Angular/src/app/history/history.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
 
-  
   constructor(private router:Router) {
   }
+  
+  ngOnInit() {}
+
 
   navigate(){
     this.router.navigate(['/calculate'])
